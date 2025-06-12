0573256499e2d345d9cfb60a3fddf23b6cef1574
Julian Mateu <julianmateu@gmail.com>
2025-06-12 23:08:34 +0100

    Improve readme, support configuring md and txt options

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # Tabdown
 
+![Tabdown Logo](./tabdown.jpg)
+
 This is a very simple Chrome extension that downloads all your open tabs into a markdown file.
 
 ## Getting started
 
-To load an unpacked extension in developer mode:
-
-1. Go to the Extensions page by entering chrome://extensions in a new tab.
+1. Clone the repository.
+1. To load the extension in unpacked developer mode, go to the Extensions page by entering chrome://extensions in a new tab.
     - Alternatively, click the Extensions menu puzzle button and select Manage Extensions at the bottom of the menu.
     - Or, click the Chrome menu, hover over More Tools, then select Extensions.
 1. Enable Developer Mode by clicking the toggle switch next to Developer mode.
--- script.js
+++ script.js
@@ -1,39 +1,86 @@
+const form = document.getElementById('download-form');
+const fileNameInput = document.getElementById('filename');
 
 function getDate() {
   return new Date().toISOString().slice(0,10);
 }
 
-function downloadDataAsTextFile(data) {
-  const blob = new Blob([data], {type: 'application/text'});
+function getDefaultFileName() {
+  return `tabdown_export_${getDate()}`
+}
+
+function downloadTextFile(data, extension) {
+  const blob = new Blob([data], {type: 'text/plain'});
   const url = URL.createObjectURL(blob);
   const a = document.createElement('a');
   a.href = url;
-  a.download = `tabdown_export_${getDate()}.md`;
+  a.download = `${fileNameInput.value}.${extension}`;
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
   URL.revokeObjectURL(url);
 }
 
-async function getTabsAsMdString() {
-  let queryOptions = {};
-
-  let tabs = await chrome.tabs.query(queryOptions);
-  data = `---
+function getMarkdownFrontmatter() {
+  return `---
 date: ${getDate()}
 ---
 `
-  for (const tab of tabs) {
-    const {title, url} = tab;
-    const mdLink = `[${title}](${url})`;
-    data += `- ${mdLink}\n`;
-    console.log(`Found tab ${mdLink}`);
-  }
-  return data;
 }
 
-console.log('Tabdown');
-getTabsAsMdString().then((data) => {
-  downloadDataAsTextFile(data);
-  console.log('done');
+async function getTabsData() {
+  const queryOptions = {};
+  const tabs = await chrome.tabs.query(queryOptions);
+  
+  const tabsData = [];
+  for (const tab of tabs) {
+    console.debug(`Found tab ${tab.title}`);
+    tabsData.push(tab);
+  }
+  return tabsData;
+}
+
+function tabToPlaintextLine(tab) {
+  const {title, url} = tab;
+  return `${title}: ${url}\n`;
+}
+
+function tabToMarkdownLine(tab) {
+  const {title, url} = tab;
+  return `- [${title}](${url})\n`
+}
+
+async function downloadTabsWithFormat(format) {
+  console.log('Downloading tabs with format', format);
+
+  const rowFormatter = format === 'markdown' ? tabToMarkdownLine : tabToPlaintextLine;
+  let text = format === 'markdown' ? getMarkdownFrontmatter() : '';
+
+  const tabsData = await getTabsData();
+
+  for (const tab of tabsData) {
+    text += rowFormatter(tab);
+  }
+
+  const extension = format === 'markdown' ? 'md' : 'txt';
+  downloadTextFile(text, extension);
+}
+
+fileNameInput.value = getDefaultFileName();
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const form = e.target;
+
+  const checkedValue = [...form.elements]
+    .filter((input) => input.checked)
+    .map((input) => input.value)[0];
+
+  console.log('Tabdown');
+  downloadTabsWithFormat(checkedValue)
+    .then(() => {
+      console.log('Done');
+    });
 });
+
